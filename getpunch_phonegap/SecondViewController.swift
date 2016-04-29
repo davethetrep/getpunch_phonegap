a7f9ba3d4970523ccaf3ffa747619c056cb16c7d
David Clark <Wiseacre@davids-macbook-air.local>
2016-04-29 18:57:51 -0500

    Initial Commit

---
+++ getpunch_phonegap/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  getpunch_phonegap
+//
+//  Created by David Clark on 4/29/16.
+//  Copyright © 2016 David Clark. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
